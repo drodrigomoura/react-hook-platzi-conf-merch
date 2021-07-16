f66acc0e235e6d1385c67908c8acdd8b7e8c3b6b
Rodrigo Moura <d.rodrigomoura@gmail.com>
2021-07-16 14:58:21 -0300

    Arquitectura de componentes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import '../styles/components/App.css';
 
-export const App = () => {
-  return (
-    <div>
-      <h1>
-        <p>Body</p>
-      </h1>
-    </div>
-  );
-};
+export const App = () => (
+  <div>
+    <h1>
+      <p>Body</p>
+    </h1>
+  </div>
+);
---
+++ src/routes/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Switch, Router, Route } from 'react-router-dom';
+import Home from '../containers/Home';
+import Checkout from '../containers/Checkout';
+import Information from '../containers/Information';
+import NotFound from '../containers/NotFound';
+import Payment from '../containers/Payment';
+import Success from '../containers/Success';
+
+export const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/checkout" component={Checkout} />
+      <Route exact path="/checkout/information" component={Information} />
+      <Route exact path="/checkout/payment" component={Payment} />
+      <Route exact path="/checkout/success" component={Success} />
+
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+);
