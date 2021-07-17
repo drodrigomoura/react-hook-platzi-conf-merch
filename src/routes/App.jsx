d4ac9dc214519fc613cf0bacdfc41643e8813b1b
Rodrigo Moura <d.rodrigomoura@gmail.com>
2021-07-17 12:58:03 -0300

    useContext y custom hooks: useInitialState

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -7,19 +7,27 @@ import NotFound from '../containers/NotFound';
 import Payment from '../containers/Payment';
 import Success from '../containers/Success';
 import Layout from '../components/Layout';
+import { AppContext } from '../context/AppContext';
+import useInitialState from '../hooks/useInitialState';
 
-export const App = () => (
-  <BrowserRouter>
-    <Layout>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route exact path="/checkout/information" component={Information} />
-        <Route exact path="/checkout/payment" component={Payment} />
-        <Route exact path="/checkout/success" component={Success} />
+export const App = () => {
+  const initialState = useInitialState();
 
-        <Route component={NotFound} />
-      </Switch>
-    </Layout>
-  </BrowserRouter>
-);
+  return (
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/checkout" component={Checkout} />
+            <Route exact path="/checkout/information" component={Information} />
+            <Route exact path="/checkout/payment" component={Payment} />
+            <Route exact path="/checkout/success" component={Success} />
+
+            <Route component={NotFound} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
+  );
+};
