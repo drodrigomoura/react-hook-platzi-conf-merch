9c6b07f202922d7c29ee9c1d126d18d3644d36dc
Rodrigo Moura <d.rodrigomoura@gmail.com>
2021-07-19 23:30:33 -0300

    Integracion de pagos con paypal - 1

--- src/containers/Payment.jsx
+++ src/containers/Payment.jsx
@@ -1,13 +1,65 @@
-import React from 'react';
-import '../styles/components/Payment.css';
+import React, { useContext } from 'react';
+import { PayPalButton } from 'react-paypal-button-v2';
 
-const Payment = () => (
-  <div className="Payment">
-    <div className="Payment-content">
-      <h3>Resumen del pedido:</h3>
-      <div className="Payment-button">Boton de pago Paypal</div>
+import { AppContext } from '../context/AppContext';
+import '../styles/components/Payment.css';
+import { handleSumTotal } from '../utils/handleSumTotal';
+
+const Payment = ({ history }) => {
+  const { state, addNewOrder } = useContext(AppContext);
+  const { cart, buyer } = state;
+
+  const paypalOptions = {
+    clientId:
+      'ARISJT7igV9j_z3dQXDUjirRiieZrHzON598FKW9eyYQeyFig2MS7MIOiIGTgYwUZ5vjaJZB-Gqha1jl',
+    intent: 'capture',
+    currency: 'USD',
+  };
+
+  const buttonStyles = {
+    layout: 'vertical',
+    shape: 'rect',
+  };
+
+  const handlePaymentSuccess = (data) => {
+    console.log(data);
+    if (data.status === 'COMPLETED') {
+      const newOrder = {
+        buyer,
+        product: cart,
+        payment: data,
+      };
+      addNewOrder(newOrder);
+      history.push('/checkout/success');
+    }
+  };
+
+  return (
+    <div className="Payment">
+      <div className="Payment-content">
+        <h3>Resumen del pedido:</h3>
+        {cart.map((item) => (
+          <div className="Payment-item" key={item.title}>
+            <div className="Payment-element">
+              <h4>{item.title}</h4>
+              <span>$ {item.price}</span>
+            </div>
+          </div>
+        ))}
+        <div className="Payment-button">
+          <PayPalButton
+            paypalOptions={paypalOptions}
+            buttonStyles={buttonStyles}
+            amount={handleSumTotal(cart)}
+            onPaymentStart={() => console.log('Start Payment')}
+            onPaymentSuccess={(data) => handlePaymentSuccess(data)}
+            onPaymentError={(error) => console.log(error)}
+            onPaymentCancel={(data) => console.log(data)}
+          />
+        </div>
+      </div>
     </div>
-    <div className="Payment-sidebar"></div>
-  </div>
-);
+  );
+};
+
 export default Payment;
